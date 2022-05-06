3d7a24750e045c5dee375431bdd9773c6fbf2bca
tony7kh <antonkhizhuk1981@gmail.com>
2022-05-06 09:51:22 +0300

    add hw 5

---
+++ students/tony7kh/hw-5/answers.js
@@ -0,0 +1,36 @@
+//Task 1
+const getMaxNumber = (...number) => result = Math.max(...number);
+console.log(getMaxNumber(2, 0));
+console.log(getMaxNumber(2, 0, 5));
+console.log(getMaxNumber(2, 0, 5, -1, 3, 10, 4));
+
+
+//Task 2
+const joinObj = (obj1, obj2, ...obj) => {
+    return student = Object.assign({}, obj1, obj2, ...obj)
+};
+console.log(joinObj({name: 'Nike'}, {title: 'boss'}, {age: 30},{position: 'CEO'}, {isDefault: false}));
+
+//Task 3
+
+const student = {
+    name: 'Anna',
+    age: 20
+};
+
+Object.defineProperties(student, 
+    {'toString': {
+    enumerable: false,
+    value: function(){
+        return `My name is ${this.name} and i'm ${this.age} years old`
+    }},
+    'valueOf': {
+        enumerable: false,
+        value: function(){
+            return this.age 
+        }
+    }
+    });
+
+console.log( 'Hello ' + student );
+console.log( person * 10);
