d17f51659793f2b9bbd43b5543c302e6d9987a02
msemerni <msemerni@gmail.com>
2022-05-23 21:35:41 +0300

    add hw-8

---
+++ students/msemerni/hw-8/js/task-3.js
@@ -0,0 +1,39 @@
+//// Task 3
+//// Substring amount
+
+const initialString = "Hello world wor";
+
+//// first
+function searchSubstringAmount1(initialString, searchingSubstring) {
+  const regexp = RegExp(searchingSubstring, "gi");
+  let result = initialString.match(regexp);
+  if (!result) {
+    // console.log(`%cThere is no "${searchingSubstring}" in the initial string`, "color: red;");
+    return `There is no "${searchingSubstring}" in the initial string`;
+  }
+  // console.log(`%c${result.length}`, "color: green;");
+  return result.length;
+}
+
+//// second
+function searchSubstringAmount2(initialString, searchingSubstring) {
+  const result = initialString.split(searchingSubstring).length - 1;
+  if (!result) {
+    // console.log(`%cThere is no "${searchingSubstring}" in the initial string`, "color: red;");
+    return `There is no "${searchingSubstring}" in the initial string`;
+  }
+  // console.log(`%c${result}`, "color: blue;");
+  return result;
+}
+
+searchSubstringAmount1(initialString, "world"); // 1
+searchSubstringAmount1(initialString, "wor"); // 2
+searchSubstringAmount1(initialString, "o"); // 3
+searchSubstringAmount1(initialString, "l"); // 3
+searchSubstringAmount1(initialString, "cat"); // There is no "cat" in the initial string
+
+searchSubstringAmount2(initialString, "world"); // 1
+searchSubstringAmount2(initialString, "wor"); // 2
+searchSubstringAmount2(initialString, "o"); // 3
+searchSubstringAmount2(initialString, "l"); // 3
+searchSubstringAmount2(initialString, "cat"); // There is no "cat" in the initial string
