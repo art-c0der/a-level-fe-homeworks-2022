c19b20062825196e688ec4887f8a76e16b88b24a
FlyStr1ker <Ketraofsaman@gmail.com>
2022-05-30 20:59:59 +0300

    add project files

---
+++ students/FlyStriker/dom-practice/script.js
@@ -0,0 +1,23 @@
+document.getElementById("defaultOpen").click();
+
+function openInstrument(active, InstrumentName) {
+    let i, tab_content, tab_links;
+
+tab_content = document.getElementsByClassName("tab_content");
+  for (i = 0; i < tab_content.length; i++) {
+     tab_content[i].style.display = "none";
+}
+
+tab_links = document.getElementsByClassName("tab_links");
+  for (i = 0; i < tab_links.length; i++) {
+     tab_links[i].className = tab_links[i].className.replace("active", "");
+}
+
+document.getElementById(InstrumentName).style.display = "block";
+ active.currentTarget.className += "active";
+}
+
+function toggleBurger(){
+  const buttonMenu = document.getElementById("button_menu_mobile");
+  buttonMenu.classList.toggle("display_burger_menu_mobile");
+}
