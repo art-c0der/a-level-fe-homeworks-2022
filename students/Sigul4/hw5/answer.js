ed7a702433cb087b3a012b45629272e3d26d3983
Sigul4 <romannamor637@gmail.com>
2022-06-04 10:02:53 +0300

    hw_5_Tsigulov

--- students/Sigul4/hw5/answer.js
+++ students/Sigul4/hw5/answer.js
@@ -1,3 +1,9 @@
+// ********************
+// 
+// Finding max elements
+// 
+// ********************
+
 //First Level
 function getMaxNumber( firstNumber, secondNumber){
     console.log(`getMaxNumber(${firstNumber},${secondNumber})= `,secondNumber > firstNumber ? secondNumber : firstNumber)  
@@ -26,3 +32,62 @@ function getMaxNumberOfCountless( ...args){
 }
 
 getMaxNumberOfCountless(8,3,9,4,2) // getMaxNumberOfCountless(...)=  9
+
+
+
+// ********************
+// 
+// Unite objects
+// 
+// ********************
+
+function joinObjects( ...objects){
+    let finalObject = {}
+    for (elem of objects){
+        finalObject = Object.assign(finalObject, elem)  
+    }
+    console.log(finalObject) 
+}
+
+obj1 = {
+    '1': 34,
+    '2': 23
+}
+obj2 = {
+    '3': 3422,
+    '4': 223
+}
+obj3 = {
+    '5': 7412,
+    '6': 723
+}
+
+joinObjects(obj1, obj2, obj3) // {1: 34, 2: 23, 3: 3422, 4: 223, 5: 7412, 6: 723}
+
+
+const student = {
+    name: "Peter",
+    age: 41,
+  };
+  
+  console.log(Object.keys(student));
+  
+  Object.defineProperties(student, {
+    toString: {
+      value: function () {
+        return `Hello, my name is ${this.name}`;
+      },
+    },
+  });
+
+  Object.defineProperties(student, {
+    valueOf: {
+      value: function () {
+        return this.age;
+      },
+    },
+  });
+  
+  alert(student); // Hello, my name is Anna
+  alert(student * 10); // 200
+  alert(student - 5); // 15
