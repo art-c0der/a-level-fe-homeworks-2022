df628a44b6f33ed6ad2369439ededd097e9a4083
Tony <a777bakaev@gmail.com>
2022-06-07 20:46:15 +0300

    corrections made

--- students/AntonBakaiev/hw-6+7/Task-1.js
+++ students/AntonBakaiev/hw-6+7/Task-1.js
@@ -1,11 +1,12 @@
 const enterNumber = prompt('Enter number');
 
 const initialNumber = () => {
-    if (enterNumber == 0) {
+    Number(enterNumber)
+    if (enterNumber === 0) {
         alert('zero');
     } else if (enterNumber % 2 === 0) {
         alert('even');
-    } else if ((enterNumber == 1) || (enterNumber % 3 === 0)) {
+    } else if ((enterNumber === 1) || (enterNumber % 3 === 0) || (enterNumber % 5 === 0) ) {
         alert('odd');
     } else alert('Not a number')
 };
