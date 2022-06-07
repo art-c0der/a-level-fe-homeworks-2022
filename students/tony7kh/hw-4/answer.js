745a1f4e78d255eb4ea2f3618f662906b0a8fa17
tony7kh <antonkhizhuk1981@gmail.com>
2022-06-07 12:22:52 +0300

    fix 4 hw

--- students/tony7kh/hw-4/answer.js
+++ students/tony7kh/hw-4/answer.js
@@ -1,24 +1,22 @@
 function searchVowels(initialString) {
-    let vowels = 'aeiouyAEIOUY';
-    let vowelsCounter = 0;
-    let normalizeString = initialString.toLowerCase() ; 
-    for(let i = 0; i < normalizeString.length ; i++) {
-        if (vowels.indexOf(normalizeString[i]) !== -1) {
-        vowelsCounter += 1;
-        }
-    
+  const vowels = 'aeiouy';
+  let vowelsCounter = 0;
+  const normalizeString = initialString.toLowerCase();
+  for (let i = 0; i < normalizeString.length; i++) {
+    if (vowels.indexOf(normalizeString[i]) !== -1) {
+      vowelsCounter += 1;
     }
-    return vowelsCounter;
+  }
+  return vowelsCounter;
 }
 
 function inputValidation(input) {
-    const validNumber = /(?:\+|\d)[\d\-\(\) ]{9,}\d/ ;
-    const validMail = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
-    const validCard = /^[3][4|7]\d{13}$|^[5][1-5]\d{14}$|^[4]\d{15}$/;
-    if ((input.match(validNumber)) || (input.match(validMail)) || (input.match(validCard))) {
-            console.log("Valid");
-        }
-        else {
-            console.log("Invalid");
-        }
+  const validNumber = /(?:\+|\d)[\d\-\(\) ]{9,}\d/;
+  const validMail =/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const validCard = /^[3][4|7]\d{13}$|^[5][1-5]\d{14}$|^[4]\d{15}$/;
+  if (input.match(validNumber) || input.match(validMail) || input.match(validCard)) {
+    console.log('Valid');
+  } else {
+    console.log('Invalid');
+  }
 }
