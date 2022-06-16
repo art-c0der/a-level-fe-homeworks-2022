375acb3e4ed138179bee804d0e94e946679a29fd
FlyStr1ker <Ketraofsaman@gmail.com>
2022-06-16 11:57:51 +0300

    add coments to js file

--- students/FlyStr1ker/hw-6-7/task-1.js
+++ students/FlyStr1ker/hw-6-7/task-1.js
@@ -1,17 +1,21 @@
 
 function initialNumber (number){
+// создал функцию для проверки числа
 
     if (number === 0){
     return 'zero'
+    // при выполнении условия возвращается строка 'zero'
     }else if(number % 2 == 0){
     return 'even'
+    // при выполнении условия возвращается строка 'even'
     }else if(number % 2 !== 0){
     return 'odd'
+    // при выполнении условия возвращается строка 'odd'
     }
 }
 
-console.log(initialNumber(6))
-console.log(initialNumber(-6))
-console.log(initialNumber(5))
-console.log(initialNumber(-5))
-console.log(initialNumber(0))
+console.log(initialNumber(6)) // выводит 'even'
+console.log(initialNumber(-6)) // выводит 'even'
+console.log(initialNumber(5)) // выводит 'odd'
+console.log(initialNumber(-5)) // выводит 'odd'
+console.log(initialNumber(0)) // выводит 'zero'
--- students/FlyStr1ker/hw-6-7/task-3.js
+++ students/FlyStr1ker/hw-6-7/task-3.js
@@ -1,4 +1,5 @@
 const numberDays = [3,4,5,1,6,2,7]
+// создаем массив для проверки дней недели
 
 numberDays[0] = "Wednesday"
 numberDays[1] = "Thursday"
@@ -7,5 +8,7 @@ numberDays[3] = "Monday"
 numberDays[4] = "Saturday"
 numberDays[5] = "Tuesday"
 numberDays[6] = "Sunday"
+// заполняем массив днями недели в правельном порядке
 
 console.log(numberDays)
+// выводим массив дней недели в строках
