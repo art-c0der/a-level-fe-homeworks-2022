a403e845d95538f94fa897153db6a534001d80bc
FlyStr1ker <Ketraofsaman@gmail.com>
2022-06-16 11:31:20 +0300

    add coments to js file

--- students/FlyStriker/hw-4/answer.js
+++ students/FlyStriker/hw-4/answer.js
@@ -1,13 +1,17 @@
 function searchVowels(string){
+    //Создаем массив гласных букв
     let count = 0;
+     //создаем переменную для подсчета гласных букв
     const vowels = ["a","e","i","o","u"];
-
+     //создаем масив необходимых елементов поиска
     for(let char of string.toLowerCase()){
         if(vowels.includes(char)){
             count += 1;
+    //создаем цикл который проверяет есть ли в масиве необходимый елемент , на каждый шаг цыкла проверяет есть ли в масиве необходимый елемент . И если есть добавляет его в переменную count
         }
     }
     return count;
+    //возвращаем значение переменной count
 }
 
 console.log(searchVowels("Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi praesentium cum excepturi tempore repellendus quos voluptatem consectetur ab, id rem nobis inventore neque ex illo, repellat doloribus fugiat mollitia accusantium"));  //85 Vowels
