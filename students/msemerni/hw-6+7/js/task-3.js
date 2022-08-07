6fe2f169f030b1a16f1174e039b9d08a75f1bfae
msemerni <49695301+msemerni@users.noreply.github.com>
2022-08-07 16:21:59 +0300

    add hw-6+7 (#110)
    * add hw-6+7
    
    * changes made

---
+++ students/msemerni/hw-6+7/js/task-3.js
@@ -0,0 +1,50 @@
+//// Task 3
+//// Transform each element in the initial array with numbers
+//// that represent days of the week to an array with human readable day names
+
+//// first
+const numberDays = [3, 4, 5, 1, 6, 2, 7];
+
+function convertNumberDays1(daysArray) {
+  let humanDays = [];
+  daysArray.forEach(day => {
+    if (day === 1) {
+      humanDays.push("Monday");
+    } else if (day === 2) {
+      humanDays.push("Tuesday");
+    } else if (day === 3) {
+      humanDays.push("Wednesday");
+    } else if (day === 4) {
+      humanDays.push("Thursday");
+    } else if (day === 5) {
+      humanDays.push("Friday");
+    } else if (day === 6) {
+      humanDays.push("Saturday");
+    } else if (day === 7) {
+      humanDays.push("Sunday");
+    }
+  });
+  // console.log(humanDays);
+  return humanDays;
+}
+
+//// second
+const humanDaysArray = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+function convertNumberDays2(daysArray, humanDaysArray) {
+  let humanDays = daysArray.map((days) => 
+    humanDaysArray[days - 1]
+  );
+  return humanDays;
+}
+
+convertNumberDays1(numberDays);
+convertNumberDays2(numberDays, humanDaysArray);
