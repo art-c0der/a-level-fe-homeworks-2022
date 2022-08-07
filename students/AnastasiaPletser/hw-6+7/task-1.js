90d5ca6b287ea1a671366f12ddfc78f430b6c38d
AnastasiaPletser <83334089+AnastasiaPletser@users.noreply.github.com>
2022-08-07 16:18:12 +0300

    add hw-6+7 (#112)

---
+++ students/AnastasiaPletser/hw-6+7/task-1.js
@@ -0,0 +1,16 @@
+// Task1
+// Write a condition to detect even or odd numbers and write the messege in console
+
+const initialNumber = Math.floor (3);
+
+if (initialNumber === 0) {
+    console.log ('zero')
+}
+
+else if (initialNumber % 2 !== 0) {
+    console.log ('odd')
+}
+
+else {
+    console.log ('even')
+}
